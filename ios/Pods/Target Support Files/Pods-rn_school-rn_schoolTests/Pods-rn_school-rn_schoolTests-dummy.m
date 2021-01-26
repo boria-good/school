d99a6a25ca25d40df1d1fe380a8a35609cf1a418
Borys Khoroshyi <boria.good@gmail.com>
2021-01-26 20:26:41 +0200

    start project

---
+++ ios/Pods/Target Support Files/Pods-rn_school-rn_schoolTests/Pods-rn_school-rn_schoolTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rn_school_rn_schoolTests : NSObject
+@end
+@implementation PodsDummy_Pods_rn_school_rn_schoolTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-rn_school-tvOSTests/Pods-rn_school-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rn_school_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_rn_school_tvOSTests
+@end
